4bffd5ef9cc7c07f07fa12fccfed6a45d143691f
eleni-tkm <68957419+eleni-tkm@users.noreply.github.com>
2024-09-10 10:36:09 +0300

    Update unmatched_DX_vegMap.py

--- unmatched_DX_vegMap.py
+++ unmatched_DX_vegMap.py
@@ -1,21 +1,21 @@
 import geopandas as gpd
-
+#HOW TO COMPARE THE FIELDS FOR TWO DIFFERENT SHAPEFILES USING GEOPANDAS (JUST ONE WAY... THYERE ARE TOO MANY THOUGHHHH :))
 # Define the file paths
-file_path1 = r"Z:\Temporary\eleni_data\TAIPED\data\SP_02_DX.shp"
-file_path2 = r"Z:\Projects\Dimosio\K207 - TAIPED\02_Antipirika_sxedia_SP02\Working_files\Data_Help_Photos\Data\4. Paragomena Stoixeia\temp_files\polygonsUnmatched.shp"
+file_path1 = r"blah\blah\firstShapefile.shp"
+file_path2 = r""blah\blah\secondShapefile.shp"
 
-# Read the shapefiles
+# Read the shapefiles with geopandas
 gdf1 = gpd.read_file(file_path1)
 gdf2 = gpd.read_file(file_path2)
 
-# Pre-create a unique identifier field in gdf2
+# Pre-create a unique identifier (id) field in gdf2
 gdf2['unique_id'] = range(len(gdf2))
 
-# Perform a spatial join
+# Perform a spatial join based on intersection (ypou can create a spatial join with the use of a tone of other geospatial relationships though, i need intersections here one to one which is the default behavior od a spatial join)
 intersections = gpd.sjoin(gdf1, gdf2, how='inner', predicate='intersects')
 
-# Define the conditions
-condition1 = (intersections['KATHGORIA'] == 'Δ') & (intersections['VEG_TYPE'].isin([1, 2, 3, 4]))
+# here I define some conditions based on field values
+condition1 = (intersections['KATHGORIA'] == 'Δ') & (intersections['VEG_TYPE'].isin([1, 2, 3, 4])) #IN (1, 2, 3, 4) for sql
 condition2 = (intersections['KATHGORIA'] == 'X') & (intersections['VEG_TYPE'] == 5)
 condition3 = (intersections['KATHGORIA'] == 'Α') & (intersections['VEG_TYPE'].isin([6, 7, 8, 9, 10]))
 condition4 = (intersections['KATHGORIA'].isin(['A', ''])) & (intersections['VEG_TYPE'] == 11)
@@ -24,14 +24,14 @@ condition4 = (intersections['KATHGORIA'].isin(['A', ''])) & (intersections['VEG_
 unmatched_conditions = ~(condition1 | condition2 | condition3 | condition4)
 unmatched_polygons_gdf2 = gdf2[gdf2['unique_id'].isin(intersections[unmatched_conditions]['unique_id'])]
 
-# Define the output file path for unmatched polygons from gdf2
-output_unmatched_path_gdf2 = r"Z:\Projects\Dimosio\K207 - TAIPED\02_Antipirika_sxedia_SP02\Working_files\Data_Help_Photos\Data\output_unmatched_gdf2.shp"
+# output file name
+output_unmatched_path_gdf2 = r"blah\blah\output_unmatched.shp"
 
 # Save the unmatched polygons from gdf2 to a new shapefile
 unmatched_polygons_gdf2.to_file(output_unmatched_path_gdf2)
 
 # Check the resulting DataFrames
-print("Intersecting Polygons:")
-print(intersections)
-print("\nUnmatched Polygons from gdf2:")
-print(unmatched_polygons_gdf2)
+#print("Intersecting Polygons:")
+#print(intersections)
+#print("\nUnmatched Polygons from gdf2:")
+#print(unmatched_polygons_gdf2)
