79c7d6a1d25578afed9d16181645dd140886c5bd
eleni-tkm <68957419+eleni-tkm@users.noreply.github.com>
2024-09-11 09:37:41 +0300

    Update CODE_FOTO_test.py

--- CODE_FOTO_test.py
+++ CODE_FOTO_test.py
@@ -1,18 +1,18 @@
 import arcpy
 
 # Set workspace
-arcpy.env.workspace = r'\\SERVER\kartECO SHARED\Temporary\eleni_data\k196\3h_Tmhmatiki\01_SAP_subFot_SP.gdb'
+arcpy.env.workspace = r'blah\blah\blah.shp'
 
 # Set feature class name
-feature_class = 'subFOTOSHMEIA_SP_working'
+feature_class = 'theNameOfFeatureClassInGDB'
 
 # Task 1: Take the feature class 'subFOTOSHMEIA_SP_working'
 # Task 2: Keep a list called 'initial_fields' with the names of the fields of the feature class 'subFOTOSHMEIA_SP_working'
 initial_fields = [field.name for field in arcpy.ListFields(feature_class)]
 
 # Task 3: Perform Spatial join
-join_features = r'\\SERVER\kartECO SHARED\Projects\Dimosio\K196 - Apografi Daswn\Working_files\Data_Help_Photos\Data\YPOERGO_8\01_DATA\02_PERIGRAMMA_FOTOSHMEIOU\YPXSX00_FOT_ypoergo_8.shp'
-output_feature_class = 'subFOTOSHMEIA_SP_working_Joined2'
+join_features = r'blah\blah\blah2.shp'
+output_feature_class = 'theNameOfFeatureClassInGDB_ToSaveResults'
 
 
 arcpy.SpatialJoin_analysis(feature_class, join_features, output_feature_class)
